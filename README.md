91e717c0585480b81addb82a02b6abd3fb96848b
Brandon C <bgpkr@att.net>
2023-11-12 22:27:00 -0600

    fix

--- README.md
+++
@@ -1,1 +0,0 @@
-# This-Or-That-Test-SITE
--- script.js
+++ script.js
@@ -1,47 +1,18 @@
-let currentGender = ""; // To store the selected gender
-let currentSeed = ""; // To store the seed
+const photoContainer = document.getElementById('photo-container');
+const displayPhoto = document.getElementById('display-photo');
+const confidenceInput = document.getElementById('confidence');
 
-function selectGender(gender) {
-  currentGender = gender;
-  refresh();
+function chooseGender(gender) {
+    const path = `/photos/${gender}`;
+    const randomIndex = Math.floor(Math.random() * 2) + 1; // Change 10 to the number of photos you have
+    const photoUrl = `${path}/photo${randomIndex}.jpg`;
+    displayPhoto.src = photoUrl;
 }
 
-function refresh() {
-  currentSeed = document.getElementById("seedInput").value;
-  const photoDisplay = document.getElementById("photoDisplay");
-
-  // Construct the image source path based on the selected gender and seed
-  const imagePath = `images/${currentGender}/${currentSeed}.jpg`; // Adjust the path based on your folder structure and file format
-
-  // Update the image source
-  photoDisplay.src = imagePath;
-}
-
-function checkAnswer() {
-  const confidenceInput = document.getElementById("confidence");
-  const confidence = confidenceInput.value;
-
-  // Validate if a gender is selected
-  if (currentGender === "") {
-    alert("Please select a gender.");
-    return;
-  }
-
-  // Validate if a confidence rating is entered
-  if (confidence === "") {
-    alert("Please enter your confidence rating.");
-    return;
-  }
-
-  // Perform any additional logic for correctness based on your requirements
-
-  // Display correctness
-  alert(`Your choice is correct.`); // You can customize this based on your correctness criteria
-
-  // Clear input fields
-  confidenceInput.value = "";
-  document.getElementById("seedInput").value = "";
-
-  // Load the next photo
-  refresh();
+function checkGuess() {
+    // Add logic to check correctness and display result
+    const userConfidence = confidenceInput.value;
+    // Add your logic to determine correctness based on user's confidence and chosen gender
+    // Update the display or show a message accordingly
+    // You can use an array of correct genders for each photo or any other method you prefer
 }
